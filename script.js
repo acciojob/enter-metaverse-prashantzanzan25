69f9de927eb7c058142ee07d908c828b2c533481
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2024-04-19 10:27:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
 //your JS code here. If required.
-document.getElementById("enterBtn").addEventListener("click", function() {
-  document.getElementById("status").textContent = "Entered Metaverse";
-});
+
 
